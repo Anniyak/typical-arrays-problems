c4bfb05ecc906366441cb03f3bc93a6306bcb80b
Anniyak <anniyak@gmail.com>
2021-07-27 13:28:38 +0300

    init: start Typical Arrays Problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (array && array.length > 0) {
+        let answer = array[0];
+        array.forEach((element) => {
+            if (element < answer) answer = element;
+        });
+        return answer;
+    } else return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array && array.length > 0) {
+        let answer = array[0];
+        array.forEach((element) => {
+            if (element > answer) answer = element;
+        });
+        return answer;
+    } else return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (array && array.length > 0) {
+        let sum = 0;
+        array.forEach((element) => {
+            sum += element;
+        });
+        return sum / array.length;
+    } else return 0;
 }
